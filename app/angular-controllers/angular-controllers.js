11457e16c190b9de92953cb441e4893d1587ef49
SkyeYeh <presto408@gmail.com>
2016-03-21 22:33:19 +0800

    Add AngularJS Controllers.

---
+++ app/angular-controllers/angular-controllers.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('myApp.angular-controllers', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/angular-controllers', {
+            templateUrl: 'angular-controllers/angular-controllers.html',
+            controller: 'AngularControllersCtrl',
+            controllerAs: 'controllers'
+        });
+    }])
+
+    // AngularControllersCtrl 函數是一個 JavaScript 函數。控制器的 $scope（相當於作用域、控制範圍）用來保存 AngularJS Model (模型)的對象。
+    .controller('AngularControllersCtrl', ['$scope', function ($scope) {
+        $scope.firstName = "John";
+        $scope.lastName = "Doe";
+
+        $scope.fullName = function () {
+            return $scope.firstName + " " + $scope.lastName;
+        };
+
+        $scope.names = [
+            {name: 'Jani', country: 'Norway'},
+            {name: 'Hege', country: 'Sweden'},
+            {name: 'Kai', country: 'Denmark'}
+        ];
+    }]);
--- app/app.js
+++ app/app.js
@@ -2,15 +2,15 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.view3',
-  'myApp.view4',
-  'myApp.directives',
-  'myApp.angular_model',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.view3',
+    'myApp.view4',
+    'myApp.directives',
+    'myApp.angular_model',
+    'myApp.angular-controllers',
+    'myApp.version'
+]).config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
