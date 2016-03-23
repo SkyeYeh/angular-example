69708ea490e6e2f2f2d0018003acb5bf7c0375ea
Skye Yeh <presto408@gmail.com>
2016-03-23 15:47:11 +0800

    Update AngularJS AJAX - $http.

--- app/angular-http/angular-http.js
+++ app/angular-http/angular-http.js
@@ -6,12 +6,33 @@ angular.module('myApp.angular-http', ['ngRoute'])
         $routeProvider.when('/angular-http', {
             templateUrl: 'angular-http/angular-http.html',
             controller: 'AngularHttpCtrl'
-    });,
-    }])
-    .controller('AngularHttpCtrl', ['$scope', '$http', function ($scope, $http) {
-        // $http 是 AngularJS 中的一個核心服務，用於讀取遠程服務器的數據。
-        $http.get('angular-http/welcome.html').then(function (res) {
-            $scope.welcome = res.data;
         });
-    }]);
+    }])
 
+    // $http 是 AngularJS 中的一個核心服務，用於讀取遠程服務器的數據。
+    .controller('AngularHttpCtrl', ['$scope', '$http', function ($scope, $http) {
+        // $http 有幾種方法：.delete()、.get()、.head()、.jsonp()、.patch()、.post()、.put()。
+        $http.get('angular-http/welcome.html').then(function (response) {
+            // Succes。
+            $scope.welcome = response.data;
+
+            // response 具有這些屬性：
+            // .config - 用來產生 request 的物件。
+            // .data - 從伺服器攜帶 response 的字串或物件。
+            // .headers - 用來獲得表頭信息的函式。
+            // .status - 定義 HTTP status 的數字。
+            // .statusText - 定義 HTTP status 的字串。
+            $scope.config = response.config;
+            $scope.data = response.data;
+            $scope.headers = response.headers;
+            $scope.status = response.status;
+            $scope.statusText = response.statusText;
+        }, function (response) {
+            // Error。
+            $scope.welcome = response.statusText;
+        });
+
+        $http.get('angular-http/customers.json').then(function (responses) {
+            $scope.records = responses.data.records;
+        });
+    }])
